20d194fdaabf78f2371822fc9abef673362ab943
thethoughtfulgeek <dalaljaineel@gmail.com>
2015-06-18 07:18:30 -0700

    Adding a modifised and optimized version of find_unique-char-str.cpp Also adding another version of find_unique-char-str-2.cpp Second version implemented in O(n) time. First version in O(nlogn) time.

--- find_unique-char-str-2.cpp
+++ find_unique-char-str-2.cpp
@@ -1,40 +1,32 @@
-/* This program implements an algorithm that determines if a string has all unique characters */
+/* This program implements another version of an algorithm that determines if a string has all unique characters 
+Here it assumed that we are not allowed to modify the string */
 
 /* Author: Jaineel Dalal <jdalal@andrew.cmu.edu>
 
-Date 06/16/2015 */
+Date 06/17/2015 */
 
 #include <iostream>
 #include <string>
 
-std::string& string_sort( std::string &string_ref )
-{
-	char temp;
-	for (int i = 1; i < string_ref.length(); ++i)
-	{
-		int j = i;
-		while ( string_ref[j] < string_ref[j-1] )
-		{
-			temp = string_ref[j];
-			string_ref[j] = string_ref[j-1];
-			string_ref[j-1] = temp;
-			j--;
-		}
-	}
-	return string_ref;
-}
-
 bool string_char_repeat( std::string &string_ref )
 {
+	bool charArray[256] = {0}; 
 	for (int i = 0; i < string_ref.length(); ++i)
 	{
-		if ( string_ref[i] == string_ref[i+1] )
+		int val = string_ref[i];
+		std::cout<<val<<'\n';
+		if ( charArray[val] == true )
 		{
 			return 0;
+		}
+		else
+		{
+			charArray[val] = true;
 		}
 	}
 	return 1;
 }
+
 int main()
 {
 	std::string str;
@@ -50,10 +42,7 @@ int main()
 			std::cout<<"\n"<<"has repeated characters"<<"\n";
 			return false;
 		}
-		std::string& sorted_str = string_sort( str );
-		is_repeat = string_char_repeat( sorted_str );
-		std::cout<<sorted_str;
-
+		is_repeat = string_char_repeat( str );
 		if (is_repeat == 0)
 		{
 			std::cout<<"\n"<<"has repeated characters"<<"\n";
@@ -63,7 +52,6 @@ int main()
 			std::cout<<"\n"<<"All unique characters"<<"\n";	
 		}
 	}
-
 	else
 	{
 		std::cout<<"\n"<<"Please enter more characters next time"<<"\n";
